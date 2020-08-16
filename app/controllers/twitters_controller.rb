c9e34375676c1c06c515dc08b75f47c70ebc48bb
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-08-16 16:52:01 +0900

    3rd commit [add]1~140字の投稿制限（エラー文含む）:

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -6,8 +6,12 @@ class TwittersController < ApplicationController
     @twitter = Twitter.new
   end
   def create
-    Twitter.create(content: params[:twitter][:content])
-    redirect_to new_twitter_path
+    @twitter = Twitter.create(content: params[:twitter][:content])
+    if @twitter.save
+      redirect_to new_twitter_path, notice: "つぶやきました！"
+    else
+      render :new
+    end
   end
   def show
     @twitter = Twitter.find(params[:id])
